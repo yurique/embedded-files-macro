2eaf09ffd4b982400fd3fdede3747995cba21a40
Iurii Malchenko <i@yurique.com>
2021-05-26 23:50:47 +0300

    * for wildcard imports

--- macro/src/main/scala-3/com/yurique/embedded/Macros.scala
+++ macro/src/main/scala-3/com/yurique/embedded/Macros.scala
@@ -1,40 +1,40 @@
 package com.yurique.embedded
 
-import scala.quoted._
+import scala.quoted.*
 
 object FileAsString:
 
   var rootPackage: String = "__embedded_files"
 
   inline def apply(inline fileName: String): String = ${ fileAsStringImpl('{fileName}) }
-  
-  def fileAsStringImpl(e: Expr[String])(using Quotes): Expr[String] = 
-    import quotes.reflect._
+
+  def fileAsStringImpl(e: Expr[String])(using Quotes): Expr[String] =
+    import quotes.reflect.*
     val fileName = e.valueOrError
     val sourceFile = Position.ofMacroExpansion.sourceFile.jpath.toString
-    internal.FileNameToClassName(fileName, sourceFile, rootPackage) match 
-      case Left(error)      => report.throwError(error)  
+    internal.FileNameToClassName(fileName, sourceFile, rootPackage) match
+      case Left(error)      => report.throwError(error)
       case Right(classPath) =>
-        (classPath ++ Seq("content")).foldLeft[Option[Term]](None) { 
+        (classPath ++ Seq("content")).foldLeft[Option[Term]](None) {
           case (None, next)        => Some(Ref(Symbol.requiredPackage(next)))
           case (Some(chain), next) => Some(Select.unique(chain, next))
         }
         .fold(report.throwError(s"invalid select path: ${classPath ++ Seq("content")}"))(_.asExprOf[String])
     end match
-    
+
 object FileAsByteArray:
   var rootPackage: String = "__embedded_files"
 
   inline def apply(inline fileName: String): Array[Byte] = ${ fileAsByteArrayImpl('{fileName}) }
-  
-  def fileAsByteArrayImpl(e: Expr[String])(using Quotes): Expr[Array[Byte]] = 
-    import quotes.reflect._
+
+  def fileAsByteArrayImpl(e: Expr[String])(using Quotes): Expr[Array[Byte]] =
+    import quotes.reflect.*
     val fileName = e.valueOrError
     val sourceFile = Position.ofMacroExpansion.sourceFile.jpath.toString
-    internal.FileNameToClassName(fileName, sourceFile, rootPackage) match 
-      case Left(error)      => report.throwError(error)  
-      case Right(classPath) => 
-        (classPath ++ Seq("content")).foldLeft[Option[Term]](None) { 
+    internal.FileNameToClassName(fileName, sourceFile, rootPackage) match
+      case Left(error)      => report.throwError(error)
+      case Right(classPath) =>
+        (classPath ++ Seq("content")).foldLeft[Option[Term]](None) {
           case (None, next)        => Some(Ref(Symbol.requiredPackage(next)))
           case (Some(chain), next) => Some(Select.unique(chain, next))
         }
