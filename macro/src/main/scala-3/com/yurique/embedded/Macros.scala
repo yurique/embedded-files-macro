36e6e2b5f9328a169dd8fd81b57b80d4445bfd1c
Scala Steward <scala_steward@virtuslab.com>
2024-02-20 19:16:11 +0000

    Reformat with scalafmt 3.8.0
    Executed command: scalafmt --non-interactive

--- macro/src/main/scala-3/com/yurique/embedded/Macros.scala
+++ macro/src/main/scala-3/com/yurique/embedded/Macros.scala
@@ -23,22 +23,22 @@ object FileAsString:
           .fold(report.throwError(s"invalid select path: ${classPath ++ Seq("content")}"))(_.asExprOf[String])
     end match
 
-object FileAsByteArray:
-  var rootPackage: String = "__embedded_files"
+  object FileAsByteArray:
+    var rootPackage: String = "__embedded_files"
 
-  inline def apply(inline fileName: String): Array[Byte] = ${ fileAsByteArrayImpl('{ fileName }) }
+    inline def apply(inline fileName: String): Array[Byte] = ${ fileAsByteArrayImpl('{ fileName }) }
 
-  def fileAsByteArrayImpl(e: Expr[String])(using Quotes): Expr[Array[Byte]] =
-    import quotes.reflect.*
-    val fileName   = e.valueOrError
-    val sourceFile = Position.ofMacroExpansion.sourceFile.jpath.toString
-    internal.FileNameToClassName(fileName, sourceFile, rootPackage) match
-      case Left(error)      => report.throwError(error)
-      case Right(classPath) =>
-        (classPath ++ Seq("content"))
-          .foldLeft[Option[Term]](None) {
-            case (None, next)        => Some(Ref(Symbol.requiredPackage(next)))
-            case (Some(chain), next) => Some(Select.unique(chain, next))
-          }
-          .fold(report.throwError(s"invalid select path: ${classPath ++ Seq("content")}"))(_.asExprOf[Array[Byte]])
-    end match
+    def fileAsByteArrayImpl(e: Expr[String])(using Quotes): Expr[Array[Byte]] =
+      import quotes.reflect.*
+      val fileName   = e.valueOrError
+      val sourceFile = Position.ofMacroExpansion.sourceFile.jpath.toString
+      internal.FileNameToClassName(fileName, sourceFile, rootPackage) match
+        case Left(error)      => report.throwError(error)
+        case Right(classPath) =>
+          (classPath ++ Seq("content"))
+            .foldLeft[Option[Term]](None) {
+              case (None, next)        => Some(Ref(Symbol.requiredPackage(next)))
+              case (Some(chain), next) => Some(Select.unique(chain, next))
+            }
+            .fold(report.throwError(s"invalid select path: ${classPath ++ Seq("content")}"))(_.asExprOf[Array[Byte]])
+      end match
